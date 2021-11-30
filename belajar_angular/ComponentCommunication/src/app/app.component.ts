8352308efcd5b99bad0feef97cb3e0f5cdeb9e05
Syizuril <syihabklayan@gmail.com>
2021-11-30 11:19:21 +0700

    proxy testing

--- belajar_angular/ComponentCommunication/src/app/app.component.ts
+++ belajar_angular/ComponentCommunication/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ComponentCommunication';
   Counter = 5
-
+ //test
   countChangedHandler(count: number){
     this.Counter = count
     console.log(count);
