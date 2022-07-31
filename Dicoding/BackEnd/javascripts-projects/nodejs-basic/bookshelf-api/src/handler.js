14abc37b5b6b1345a23ae9510ecdbf51196c749d
Syizuril <syihabklayan@gmail.com>
2022-07-31 16:23:45 +0700

    init

---
+++ Dicoding/BackEnd/javascripts-projects/nodejs-basic/bookshelf-api/src/handler.js
@@ -0,0 +1,55 @@
+const {nanoid} = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (req, h) => {
+  const {name, year, author, summary, publisher, pageCount, readPage, reading} =
+    req.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+
+  response.code(500);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+};
